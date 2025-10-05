bc4a797d50bcaf69a4fa9a63a810c8dfeec48b20
Danielle Neves <dani@aranha.dev>
2025-10-05 16:18:01 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,16 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const dailyPrice = 40;
+  let totalPrice = days * dailyPrice;
+
+  if (days >= 7) {
+    totalPrice -= 50;
+  } else if (days >= 3) {
+    totalPrice -= 20;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
